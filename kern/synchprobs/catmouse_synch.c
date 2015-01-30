fb621895403376a2a41598decdf82ef2af50b16e
Winston Wu <winxton@gmail.com>
2015-01-30 17:32:39 -0500

    working cat mouse sync

--- kern/synchprobs/catmouse_synch.c
+++ kern/synchprobs/catmouse_synch.c
@@ -34,6 +34,10 @@ static struct cv *cat_cv = NULL;
 // used to make sure only ONE bowl is being consumed at once
 static struct cv **bowl_cvs = NULL;
 
+// actual variables storing whether a bowl is being eaten
+// volatile because this is being changed by multiple threads
+static volatile bool* bowls_being_eaten = NULL;
+
 /* 
  * The CatMouse simulation will call this function once before any cat or
  * mouse tries to each.
@@ -46,8 +50,6 @@ void
 catmouse_sync_init(int bowls)
 {
   /* replace this default implementation with your own implementation of catmouse_sync_init */
-
-  (void)bowls; /* keep the compiler from complaining about unused parameters */
 
   // keeps a count of the number of mice currently eating
   num_mice_eating = sem_create("num_mice_eating",0);
@@ -77,9 +79,18 @@ catmouse_sync_init(int bowls)
   }
 
   // create bowl cvs
-  bowl_cvs = malloc(bowls * sizeof(struct cv *));
-  for (int i=0; i<bowls; i++) {
+  bowl_cvs = kmalloc(bowls * sizeof(struct cv *));
+  int i = 0;
+  for (i=0; i<bowls; i++) {
     bowl_cvs[i] = cv_create("bowl_cv");;
+    if (bowl_cvs[i] == NULL) {
+      panic("bowl_cvs cv_create failed\n");
+    }
+  }
+
+  bowls_being_eaten = kmalloc(bowls * sizeof(volatile bool));
+  for (i=0; i<bowls; i++) {
+    bowls_being_eaten[i] = false;
   }
 
   return;
@@ -113,7 +124,8 @@ catmouse_sync_cleanup(int bowls)
   for (int i=0; i<bowls; i++) {
     cv_destroy(bowl_cvs[i]);
   }
-  free(bowl_cvs);
+  kfree(bowl_cvs);
+  kfree((void *)bowls_being_eaten); // need to cast because of volatile
 }
 
 
@@ -133,17 +145,44 @@ void
 cat_before_eating(unsigned int bowl) 
 {
   /* replace this default implementation with your own implementation of cat_before_eating */
-  (void)bowl;  /* keep the compiler from complaining about an unused parameter */
   KASSERT(num_mice_eating != NULL && num_cats_eating != NULL);
 
   // critical section: this is needed for the conditional variable check on the number of eating mice
   lock_acquire(mutex);
 
-    // oh no! a mouse is currently eating
-    while (num_mice_eating->sem_count > 0) {
-      cv_wait(cat_cv, mutex); // this would release the lock and sleep until awaken
+    kprintf("Cat WAITS at bowl: %d\n", bowl);
+    // oh no! A mouse is currently eating
+
+    bool can_continue = false;
+    while (!can_continue) {
+
+      while (num_mice_eating->sem_count != 0) {
+        cv_wait(cat_cv, mutex); // this would release the lock and sleep until awaken
+      }
+
+      // oh no! Current bowl is being eaten: guaranteed to be by another cat by above
+      while (bowls_being_eaten[bowl-1]) {
+        cv_wait(bowl_cvs[bowl-1], mutex);
+      }
+
+      // A mouse can win the race condition, wait for that to finish
+      // e.g. Mouse-1 is eating bowl 1, with both a Cat (waiting on any bowl) and Mouse-2 (waiting on bowl 1) waiting to enter the CS.
+      // After Mouse-1 completes and signals, the mouse wins the race and enters the CS, 
+      // but num_mice_eating could be 1 (instead of 0 that was checked earlier with the cat_cv variable)
+      // => Need to loop to repeat the check
+
+      can_continue = num_mice_eating->sem_count == 0;
     }
-    
+
+    kprintf("Cat EATS bowl: %d\n", bowl);
+
+    // should never happen
+    KASSERT(num_mice_eating->sem_count == 0);
+    KASSERT(!bowls_being_eaten[bowl-1]);
+
+    // I am now eating this bowl
+    bowls_being_eaten[bowl-1] = true;
+
     // increase the number of cats that are eating
     V(num_cats_eating);
 
@@ -167,13 +206,17 @@ void
 cat_after_eating(unsigned int bowl) 
 {
   /* replace this default implementation with your own implementation of cat_after_eating */
-  (void)bowl;  /* keep the compiler from complaining about an unused parameter */
   KASSERT(num_mice_eating != NULL && num_cats_eating != NULL);
 
   lock_acquire(mutex);
-    
+
+    kprintf("Cat exits bowl: %d\n", bowl);
+
     // decrease number of eating cats
     P(num_cats_eating);
+
+    bowls_being_eaten[bowl-1] = false;
+    cv_signal(bowl_cvs[bowl-1], mutex);
 
     // no more cats are eating, 
     // so I broadcast to all waiting mice that they can start eating
@@ -200,15 +243,37 @@ void
 mouse_before_eating(unsigned int bowl) 
 {
   /* replace this default implementation with your own implementation of mouse_before_eating */
-  (void)bowl;  /* keep the compiler from complaining about an unused parameter */
+
   KASSERT(num_mice_eating != NULL && num_cats_eating != NULL);
 
   lock_acquire(mutex);
 
-    // oh no! a cat is currently eating, I must wait!
-    while (num_cats_eating->sem_count > 0) {
-      cv_wait(mice_cv, mutex); // this would release the lock and sleep until awaken
+    kprintf("Mouse WAITS at bowl: %d\n", bowl);
+
+    bool can_continue = false;
+    while (!can_continue) {
+      // oh no! a cat is currently eating, I must wait!
+      while (num_cats_eating->sem_count != 0) {
+        cv_wait(mice_cv, mutex); // this would release the lock and sleep until awaken
+      }
+
+      // oh no! Current bowl is being eaten: guaranteed to be by another mouse by above
+      while (bowls_being_eaten[bowl-1]) {
+       cv_wait(bowl_cvs[bowl-1], mutex);
+      }
+
+      // A cat could win the race condition, need to continue waiting
+      can_continue = num_cats_eating->sem_count == 0;
     }
+
+    kprintf("Mouse EATS bowl: %d\n", bowl);
+
+    // should never happen
+    KASSERT(num_cats_eating->sem_count == 0);
+    KASSERT(!bowls_being_eaten[bowl-1]);
+
+    // I am now eating this bowl
+    bowls_being_eaten[bowl-1] = true;
 
     // increase the number of mice that are eating
     V(num_mice_eating);
@@ -233,13 +298,18 @@ void
 mouse_after_eating(unsigned int bowl) 
 {
   /* replace this default implementation with your own implementation of mouse_after_eating */
-  (void)bowl;  /* keep the compiler from complaining about an unused parameter */
+
   KASSERT(num_mice_eating != NULL && num_cats_eating != NULL);
 
   lock_acquire(mutex);
 
+    kprintf("Mouse exits bowl: %d\n", bowl);
+
     // decrease number of eating mice
     P(num_mice_eating);
+
+    bowls_being_eaten[bowl-1] = false;
+    cv_signal(bowl_cvs[bowl-1], mutex);
 
     // if no more mice are eating, then I broadcast to all waiting cats that they can start eating
     if (num_mice_eating->sem_count == 0) {
